44ce9a81f699e915ac6267bfb6823f9dc66599e4
C0DE-SLAYER <fsali315@gmail.com>
2022-09-25 17:52:39 +0530

    commit flask api code

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template, request
+import requests
+import pandas as pd
+import datetime as dt
+import yfinance as yf
+from prophet import Prophet
+
+app = Flask(__name__)
+
+def get_coin_name():
+    url = 'https://api.nomics.com/v1/currencies/ticker?key=e7e8d3665fd7a6a32be426648c149ce50325fdcd'
+    data = requests.get(url).json()
+    df = pd.DataFrame(data)
+    df = df['symbol']
+    return df
+
+def ml_model(coin_name,days):
+    df = yf.download(f'{coin_name}-USD',dt.date.today() - dt.timedelta(days=365),dt.date.today())
+    df.reset_index(inplace=True)
+    df = df[['Date','Adj Close']]
+    df.columns = ['ds','y']
+    model = Prophet()
+    model.fit(df)
+    future_dates = model.make_future_dataframe(periods = days)
+    prediction = model.predict(future_dates)
+    prediction = prediction[['ds','yhat']]
+    return prediction['yhat'].iloc[-1]
+
+@app.route('/', methods = ['GET','POST'])
+def index():
+    if request.method == 'POST':
+        coin_name = request.form.get('coins')
+        days = int(request.form.get('date_pre'))
+        prediction = ml_model(coin_name,days)
+        return f'The predicted value is {prediction}'
+    else:
+        coin = get_coin_name()
+        return render_template('index.html',coin=coin)
+
+if __name__ == '__main__':
+    app.run(debug=True)
